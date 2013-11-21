789d2d793431583d566b9d927f3c639f4a62c4d4
EddieChoi <faceoff9110@gmail.com>
2013-11-21 15:34:26 +0900

    2013년 9월 26일 (5주차)
    챕터5

---
+++ 20130926/Account.java
@@ -0,0 +1,22 @@
+
+public class Account {
+    //1. 멤버 변수
+	String accountNo;
+	String ownerName;
+	int balance;
+    //2. 생성자
+	Account(){
+	}
+	
+    //3. 멤버 메소드
+	void deposit(int account){
+		this.balance += account;
+	}
+	
+	int withdraw(int account){
+		if(this.balance < account){
+			return 0;
+	    }
+	    return this.balance -= account;
+    }
+}
